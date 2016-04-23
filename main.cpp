6da436113d6f0c3745d971932741a451ab778f21
EwanC <ewan.cr@gmail.com>
2016-04-23 16:14:58 +0100

    clang-format and change input to be uint32_t not float

--- main.cpp
+++ main.cpp
@@ -1,76 +1,93 @@
-#include <iostream>
-#include <iomanip>
+/*
+    metaFloat
+    Author: ewan.cr@gmail.com
+    License: MIT
+
+    Takes an IEE754 single precision float and
+    prints the decimal string representation.
+
+    clang-format google style
+
+*/
 #include <bitset>
+#include <iomanip>
+#include <iostream>
 
 #ifndef FLOAT_VAL
-#define FLOAT_VAL 1.753f;
+// -1.234
+#define FLOAT_VAL 0xbf9df3b6
 #endif
 
+namespace {  // anonymous
 
-template <uint32_t sign, uint32_t exponent, uint32_t mantissa>
-class IEEE_754 { 
- private:
-    enum
-    colour_codes_e {
-        console_red = 31,
-        console_green = 32,
-        console_blue = 34,
-        console_default = 39
-    };
-
-    template<colour_codes_e colour>
-    void print_colour(const char* c_str)
-    {
-       std::cout << "\033[" << colour << "m";
-       std::cout << c_str;
-       std::cout << "\033[" << console_default << "m";
-    }
-
-    template<colour_codes_e colour, size_t bitset_size>
-    void print_colour(std::bitset<bitset_size> bitset)
-    {
-       std::cout << "\033[" << colour << "m";
-       std::cout << bitset;
-       std::cout << "\033[" << console_default << "m";
-    } 
- public:
-    static const uint8_t sign_bits = 1;
-    static const uint8_t exponent_bits = 8;
-    static const uint8_t mantissa_bits = 23;
-    
-    void print() {
-
-       print_colour<console_red>("Sign ");
-       print_colour<console_blue>("Exponent ");
-       print_colour<console_green>("Mantissa ");
-       std::cout << std::endl;
-
-       print_colour<console_red, sign_bits>(std::bitset<sign_bits>(sign >> (32 - sign_bits)));
-       print_colour<console_blue, exponent_bits>(std::bitset<exponent_bits>(exponent >> mantissa_bits));
-       print_colour<console_green, mantissa_bits>(std::bitset<mantissa_bits>(mantissa));
-       std::cout << std::endl;
-
-    }
-};
-
+static const uint8_t sign_bits = 1;
+static const uint8_t exponent_bits = 8;
+static const uint8_t mantissa_bits = 23;
 
 // SEEEEEEE EMMMMMMM MMMMMMMM MMMMMMMM
 // 00       7F       FF       FF       Mantissa MASK
 // 80       00       00       00       Sign MASK
 // 7F       80       00       00       Exponent MASK
-//
-int main()
-{
-   constexpr const float f = FLOAT_VAL;
+static const uint32_t sign_mask = 0x80000000;
+static const uint32_t exponent_mask = 0x7F800000;
+static const uint32_t mantissa_mask = 0x007FFFFF;
 
-   std::cout  << f << std::endl;
-   //constexpr const uint32_t cast = static_cast<uint32_t>(f);
-   const uint32_t cast = 0xbf9df3b6; // -1.234
+}  // end anonymous namespace
 
-   const uint32_t exponent = cast & (0x7F800000);
-   const uint32_t sign = cast & (0x80000000);
-   const uint32_t mantissa = cast & (0x007FFFFF);
+template <uint32_t sign, uint32_t exponent, uint32_t mantissa>
+class IEEE_754 {
+ private:
+  enum colour_codes_e {
+    console_red = 31,
+    console_green = 32,
+    console_blue = 34,
+    console_default = 39
+  };
 
-   IEEE_754<sign, exponent, mantissa> printer;
-   printer.print();
+  template <colour_codes_e colour>
+  void print_colour(const char* c_str) {
+    std::cout << "\033[" << colour << "m";
+    std::cout << c_str;
+    std::cout << "\033[" << console_default << "m";
+  }
+
+  template <colour_codes_e colour, size_t bitset_size>
+  void print_colour(std::bitset<bitset_size> bitset) {
+    std::cout << "\033[" << colour << "m";
+    std::cout << bitset;
+    std::cout << "\033[" << console_default << "m";
+  }
+
+  // TODO: Implement this
+  constexpr const char* calc_float_str() const { return "-1.234"; }
+
+  const char* m_float_str;
+
+ public:
+  IEEE_754() { m_float_str = calc_float_str(); }
+  void print() {
+    print_colour<console_red>("Sign ");
+    print_colour<console_blue>("Exponent ");
+    print_colour<console_green>("Mantissa ");
+    std::cout << std::endl;
+
+    print_colour<console_red, sign_bits>(std::bitset<sign_bits>(sign));
+    print_colour<console_blue, exponent_bits>(
+        std::bitset<exponent_bits>(exponent));
+    print_colour<console_green, mantissa_bits>(
+        std::bitset<mantissa_bits>(mantissa));
+    std::cout << std::endl;
+
+    std::cout << m_float_str << std::endl;
+  }
+};
+
+int main() {
+  constexpr const uint32_t float_rep = FLOAT_VAL;
+  const uint32_t sign = (float_rep & sign_mask) >> (32 - sign_bits);
+  const uint32_t exponent = (float_rep & exponent_mask) >> mantissa_bits;
+  const uint32_t mantissa = float_rep & mantissa_mask;
+
+  IEEE_754<sign, exponent, mantissa> printer;
+  printer.print();
 }
