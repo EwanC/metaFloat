c6225c24d5d95a63a60ac4b7c2460e6ea23e9176
EwanC <ewan.cr@gmail.com>
2016-04-28 20:11:09 +0100

    tweak comments

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <iomanip>
 #include <iostream>
 
+// The input float value to print can be set at compile time
+// with CXX_FLAGS=-DFLOAT_VALUE=0x.....
+// Otherwise a predefined value will be used.
 #ifndef FLOAT_VAL
-#define FLOAT_VAL 0xc32d3be7 // -173.234
+#define FLOAT_VAL 0xc32d3be7   // -173.234
 //#define FLOAT_VAL 0x00000000 // +0
 //#define FLOAT_VAL 0x80000000 // -0
 //#define FLOAT_VAL 0x7f800000 // +inf
@@ -14,21 +17,23 @@
 namespace
 { // anonymous
 
-// sign * 2^exponent * mantissa
+// -------------------------------------------------
+// 32 Bit layout of IEEE 754 single precision float
+// -------------------------------------------------
+// SEEEEEEE EMMMMMMM MMMMMMMM MMMMMMMM
+// 80       00       00       00       Sign MASK
+// 7F       80       00       00       Exponent MASK
+// 00       7F       FF       FF       Mantissa MASK
+// -------------------------------------------------
 static const uint8_t sign_bits = 1;
 static const uint8_t exponent_bits = 8;
 static const uint8_t mantissa_bits = 23;
 
-// SEEEEEEE EMMMMMMM MMMMMMMM MMMMMMMM
-// 00       7F       FF       FF       Mantissa MASK
-// 80       00       00       00       Sign MASK
-// 7F       80       00       00       Exponent MASK
 static const uint32_t sign_mask = 0x80000000;
 static const uint32_t exponent_mask = 0x7F800000;
 static const uint32_t mantissa_mask = 0x007FFFFF;
 
-static const uint16_t buffer_size = 256;
-
+// ANSI colour codes for UNIX consoles
 enum colour_codes_e
 {
     console_red = 31,
@@ -38,6 +43,7 @@ enum colour_codes_e
     console_default = 39
 };
 
+// Prints c string to stdout with templated ANSI colour
 template <colour_codes_e colour> void print_colour(const char* c_str)
 {
     std::cout << "\033[" << colour << "m";
@@ -45,6 +51,7 @@ template <colour_codes_e colour> void print_colour(const char* c_str)
     std::cout << "\033[" << console_default << "m";
 }
 
+// Prints individual bits to stdout with templated ANSI colour
 template <colour_codes_e colour, size_t bitset_size> void print_colour(std::bitset<bitset_size> bitset)
 {
     std::cout << "\033[" << colour << "m";
@@ -52,6 +59,7 @@ template <colour_codes_e colour, size_t bitset_size> void print_colour(std::bits
     std::cout << "\033[" << console_default << "m";
 }
 
+static const uint16_t buffer_size = 256;
 } // end anonymous namespace
 
 template <uint32_t sign, uint32_t exponent, uint32_t mantissa> class IEEE_754
@@ -71,6 +79,7 @@ template <uint32_t sign, uint32_t exponent, uint32_t mantissa> class IEEE_754
     static unsigned int s_buffer_index;
 
   public:
+    // sign * 2^exponent * mantissa
     constexpr IEEE_754()
     {
         if (sign)
@@ -80,7 +89,7 @@ template <uint32_t sign, uint32_t exponent, uint32_t mantissa> class IEEE_754
         // exponent has a bias of 127, which we must subtract
         const int32_t adjusted_exp = static_cast<int32_t>(exponent) - 127;
 
-        //// Hidden 24th bit is 1 in mantissa
+        // Hidden 24th bit is 1 in mantissa
         const uint32_t full_mantissa = mantissa | (1 << mantissa_bits);
 
         const uint32_t adjustment = mantissa_bits - adjusted_exp;
@@ -90,10 +99,10 @@ template <uint32_t sign, uint32_t exponent, uint32_t mantissa> class IEEE_754
         print_int_as_string(integer_component);
         s_printable_buffer[s_buffer_index++] = '.';
 
-        //// Top of the fraction
+        // Top of the fraction
         uint32_t frac = full_mantissa & ((1 << adjustment) - 1);
 
-        //// Base of the fraction, must be bigger than frac
+        // Base of the fraction, must be bigger than frac
         uint32_t base = 1 << adjustment;
 
         int decimal_places = 0;
@@ -132,7 +141,7 @@ unsigned int IEEE_754<sign, exponent, mantissa>::s_buffer_index = 0;
 template <uint32_t sign, uint32_t exponent, uint32_t mantissa>
 char IEEE_754<sign, exponent, mantissa>::s_printable_buffer[buffer_size] = {0};
 
-template <uint32_t sign> struct IEEE_754<sign, 0u, 0>
+template <uint32_t sign> struct IEEE_754<sign, 0u, 0u>
 {
     void print()
     {
@@ -197,23 +206,23 @@ template <uint32_t sign, uint32_t mantissa> struct IEEE_754<sign, 255u, mantissa
 
 int main()
 {
-    constexpr const uint32_t float_rep = FLOAT_VAL;
+    // Assign macro to a local variable for convenience
+    constexpr const uint32_t float_bits = FLOAT_VAL;
 
     // Get sign bit
-    const uint32_t sign = (float_rep & sign_mask) >> (32 - sign_bits);
+    const uint32_t sign = (float_bits & sign_mask) >> (32 - sign_bits);
 
     // Get 8 exponents bits
-    const uint32_t exponent = (float_rep & exponent_mask) >> mantissa_bits;
+    const uint32_t exponent = (float_bits & exponent_mask) >> mantissa_bits;
 
     // Get 23 mantissa bits
-    const uint32_t mantissa = float_rep & mantissa_mask;
+    const uint32_t mantissa = float_bits & mantissa_mask;
 
-    // Get 23 mantissa bits
-    // Instantiate print helper class
-    IEEE_754<sign, exponent, mantissa> printer;
+    // Instantiate helper class for printing the float
+    IEEE_754<sign, exponent, mantissa> ieee754_printer;
 
-    // Print to stdout at runtime
-    printer.print();
+    // Print float to stdout at runtime
+    ieee754_printer.print();
 
     return 0;
 }
